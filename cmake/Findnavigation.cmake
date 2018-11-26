f7ada2eb166437e75a6e33b459a93510df4ba749
martin <m.huber_1994@hotmail.de>
2018-11-26 13:39:54 +0100

    added vehicle plugins

---
+++ cmake/Findnavigation.cmake
@@ -0,0 +1,21 @@
+# Find the navigation library.
+find_path(NAVIGATION_INCLUDE_DIR navigation.h ${NAVIGATION_DIR}/include/navigation)
+
+find_library(NAVIGATION_LIBRARIES
+    NAMES navigation
+    PATHS ${NAVIGATION_DIR}/lib
+) 
+
+if (NAVIGATION_INCLUDE_DIR AND NAVIGATION_LIBRARIES)
+   set(NAVIGATION_FOUND TRUE)
+endif ()
+
+if (NAVIGATION_FOUND)
+  if (NOT NAVIGATION_FIND_QUIETLY)
+    message(STATUS "Found navigation: ${NAVIGATION_LIBRARIES}")
+  endif ()
+else (NAVIGATION_FOUND)
+  if (NAVIGATION_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find navigation")
+  endif ()
+endif ()
