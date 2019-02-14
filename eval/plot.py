89f0617e16d6e8150fac6651e6e64cab3b8d6648
martin <m.huber_1994@hotmail.de>
2019-02-14 20:43:31 +0100

    added support for real data

--- eval/plot.py
+++ eval/plot.py
@@ -7,20 +7,31 @@ from scipy import signal # convolution
 from utils import disc, bin_data
 
 # load data
-data = np.genfromtxt("circular_random_sample.csv", delimiter=",")
+uniform = np.genfromtxt("circular_random_sample.csv", delimiter=",")
+path = np.genfromtxt("../data/vehicle_positions.csv", delimiter=",")
+other = np.genfromtxt("../data/goal_obstacle_positions.csv", delimiter=",")
+
+goal = other[:,0:3]
+obstacle = other[:,3:6]
+
+bin_size = 1/10
 
 # bin data
-binned = bin_data(data, 1/100, 200, 200, True)
+#binned = bin_data(uniform, 1/100, 0, 0, 200, 200, True)
+binned = bin_data(obstacle[:, 0:2], bin_size=bin_size, center_x=2, center_y=0, padding_x=20, padding_y=20, normalize=True) # now with read instead of fake data
 
 # create circle
-disc = disc(100, 100)
+disc = disc(int(1/bin_size), int(1/bin_size))
 
 # convolve
 conv = signal.convolve2d(binned, disc, boundary="symm", mode="same")
 
 # plot dots
-plt.scatter(data[:, 0], data[:, 1])
-plt.plot()
+plt.scatter(obstacle[:, 0], obstacle[:, 1])
+plt.show()
+
+# plot paths
+plt.plot(path[:,0], path[:,1])
 plt.show()
 
 # plot convolution
--- eval/utils.py
+++ eval/utils.py
@@ -18,17 +18,22 @@ def disc(r, n):
 
 
 # bin data
-def bin_data(data, bin_size, padding_x, padding_y, normalize = True):
+def bin_data(data, bin_size, center_x, center_y, padding_x, padding_y, normalize = True):
 
     inv_bin_size = int(1/bin_size)
+    x_center_in_dx = int(center_x/bin_size)
+    y_center_in_dy = int(center_y/bin_size)
 
-    binned = np.zeros([inv_bin_size + 1 + padding_x, inv_bin_size + 1 + padding_y])
+    binned = np.zeros([inv_bin_size + 1 + padding_x + x_center_in_dx, inv_bin_size + 1 + padding_y + y_center_in_dy])
     offset_x = int(padding_x/2)
     offset_y = int(padding_y/2)
 
     for pair in data:
 
         x, y = int(np.round(inv_bin_size*pair[0])), int(np.round(inv_bin_size*pair[1]))
+        #print("x:    ", x, "y:    ", y)
+        #print("pair: ", pair[0], "pair: ", pair[1])
+        #print(binned.shape)
         binned[x+offset_x, y+offset_y] += 1
 
     if (normalize):
